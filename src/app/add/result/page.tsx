921533363514241199cf02fdf0ab34724a2b31a5
DaeseungLee <daeseunglee6511@gmail.com>
2024-07-17 20:46:41 +0900

    sketch menu

--- src/app/add/result/page.tsx
+++ src/app/add/result/page.tsx
@@ -36,25 +36,29 @@ function ResultPage() {
       //   console.log('prompt', prompt);
       //   // setIsModalVisible(true);
 
-      try {
-        const response = await fetch('/api/generateImage', {
-          method: 'POST'
-        });
-        console.log(response)
-        if (!response.ok) {
-          throw new Error('Fetch failed');
+      if (generateType === 'prompt') {
+
+        try {
+          const response = await fetch('/api/generateImage', {
+            method: 'POST'
+          });
+          console.log(response)
+          if (!response.ok) {
+            throw new Error('Fetch failed');
+          }
+
+          const data = await response.json();
+          console.log(data);
+
+          setImageSrc(`data:image/png;base64,${data.artifacts[0].base64}`);
+          setIsModalVisible(false);
+
+        } catch (error) {
+          console.error('Error fetching data:', error);
         }
-
-        const data = await response.json();
-        console.log(data);
-
-        setImageSrc(`data:image/png;base64,${data.artifacts[0].base64}`);
-        setIsModalVisible(false);
-
-      } catch (error) {
-        console.error('Error fetching data:', error);
+      } else {
+        console.log('sketch');
       }
-      // }
     };
 
     fetchData();
@@ -62,138 +66,138 @@ function ResultPage() {
 
 
   return (
-      <div>
-        <div className="px-3">
-          <Header title="AI 제작하기" />
+    <div>
+      <div className="px-3">
+        <Header title="AI 제작하기" />
+
+        <div
+          className="flex relative w-full bg-white rounded-2xl mt-4"
+          style={{
+            paddingTop: `${(340 / 363) * 100}%`,
+            // border: '2px solid var(--main-color)',
+          }}
+        >
+          <div
+            className="flex flex-row absolute w-full h-full rounded-xl top-0 backdrop-blur-md px-2.5 py-1 overflow-hidden"
+            style={{ border: '2px solid var(--main-color)' }}
+          >
+            {imageSrc ? (
+              <Image
+                src={imageSrc}
+                alt="Generated Image"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-md"
+              />
+            ) : (<Image
+              src='/images/add/result/sampleImage.png'
+              alt='heart icon'
+              layout="fill"  // 이미지를 부모 요소의 크기에 맞게 확장
+              objectFit="cover"  // 이미지를 부모 요소에 맞게 잘라서 보여줌
+              className="rounded-md"  // 원하는 스타일 클래스 추가
+            />
+            )}
+
+          </div>
 
           <div
-            className="flex relative w-full bg-white rounded-2xl mt-4"
-            style={{
-              paddingTop: `${(340 / 363) * 100}%`,
-              // border: '2px solid var(--main-color)',
-            }}
+            className="flex flex-row absolute w-full top-0 py-1 px-2"
           >
             <div
-              className="flex flex-row absolute w-full h-full rounded-xl top-0 backdrop-blur-md px-2.5 py-1 overflow-hidden"
-              style={{ border: '2px solid var(--main-color)' }}
+              className="py-1 px-3 rounded-md text-bold"
+              style={{ backgroundColor: '#F5EFE1', border: '1px solid #BFA054', color: '#787486', fontSize: 7 }}
             >
-              {imageSrc ? (
-                <Image
-                  src={imageSrc}
-                  alt="Generated Image"
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-md"
-                />
-              ) : (<Image
-                src='/images/add/result/sampleImage.png'
+              이전으로
+            </div>
+
+            <div
+              className="py-1 px-3 rounded-md ms-1 text-bold"
+              style={{ backgroundColor: '#F5EFE1', border: '1px solid #BFA054', color: '#787486', fontSize: 7 }}
+            >
+              업스케일
+            </div>
+
+
+          </div>
+
+
+
+          <div
+            className="flex flex-row justify-center  absolute w-full rounded-b-xl bottom-0 backdrop-blur-md px-2.5 py-2"
+            style={{ backgroundColor: 'rgba(14, 214, 208, 0.4)' }}
+          >
+            <div
+              className="flex flex-col justify-center items-center"
+            >
+              <Image
+                src='/images/add/result/copyIcon.svg'
                 alt='heart icon'
-                layout="fill"  // 이미지를 부모 요소의 크기에 맞게 확장
-                objectFit="cover"  // 이미지를 부모 요소에 맞게 잘라서 보여줌
-                className="rounded-md"  // 원하는 스타일 클래스 추가
+                width={12}
+                height={15}
               />
-              )}
-
+              <div className="mt-0.5text-regular" style={{ fontSize: 11, color: '#fff' }}>copy</div>
             </div>
 
             <div
-              className="flex flex-row absolute w-full top-0 py-1 px-2"
+              className="flex flex-col justify-center items-center mt-0.5"
+              style={{ marginLeft: 40, marginRight: 40 }}
             >
-              <div
-                className="py-1 px-3 rounded-md text-bold"
-                style={{ backgroundColor: '#F5EFE1', border: '1px solid #BFA054', color: '#787486', fontSize: 7 }}
-              >
-                이전으로
-              </div>
-
-              <div
-                className="py-1 px-3 rounded-md ms-1 text-bold"
-                style={{ backgroundColor: '#F5EFE1', border: '1px solid #BFA054', color: '#787486', fontSize: 7 }}
-              >
-                업스케일
-              </div>
-
-
+              <Image
+                src='/images/add/result/downloadIcon.svg'
+                alt='heart icon'
+                width={14}
+                height={15}
+              />
+              <div className="mt-0.5 text-regular" style={{ fontSize: 11, color: '#fff' }}>Download</div>
             </div>
-
-
 
             <div
-              className="flex flex-row justify-center  absolute w-full rounded-b-xl bottom-0 backdrop-blur-md px-2.5 py-2"
-              style={{ backgroundColor: 'rgba(14, 214, 208, 0.4)' }}
+              className="flex flex-col justify-center items-center"
             >
-              <div
-                className="flex flex-col justify-center items-center"
-              >
-                <Image
-                  src='/images/add/result/copyIcon.svg'
-                  alt='heart icon'
-                  width={12}
-                  height={15}
-                />
-                <div className="mt-0.5text-regular" style={{ fontSize: 11, color: '#fff' }}>copy</div>
-              </div>
-
-              <div
-                className="flex flex-col justify-center items-center mt-0.5"
-                style={{ marginLeft: 40, marginRight: 40 }}
-              >
-                <Image
-                  src='/images/add/result/downloadIcon.svg'
-                  alt='heart icon'
-                  width={14}
-                  height={15}
-                />
-                <div className="mt-0.5 text-regular" style={{ fontSize: 11, color: '#fff' }}>Download</div>
-              </div>
-
-              <div
-                className="flex flex-col justify-center items-center"
-              >
-                <Image
-                  src='/images/add/result/heartIcon.svg'
-                  alt='heart icon'
-                  width={17}
-                  height={15}
-                />
-                <div className="mt-0.5 text-regular" style={{ fontSize: 11, color: '#fff' }}>save</div>
-              </div>
-
-
-
+              <Image
+                src='/images/add/result/heartIcon.svg'
+                alt='heart icon'
+                width={17}
+                height={15}
+              />
+              <div className="mt-0.5 text-regular" style={{ fontSize: 11, color: '#fff' }}>save</div>
             </div>
 
+
+
           </div>
 
-          <div className="mt-3 grid grid-cols-4 gap-1">
-            <img src='/images/add/result/sampleImage.png' alt='1' className='w-full h-full rounded-md' />
-            <img src='/images/add/result/sampleImage.png' alt='1' className='w-full h-full rounded-md' />
-            <img src='/images/add/result/sampleImage.png' alt='1' className='w-full h-full rounded-md' />
-            <img src='/images/add/result/sampleImage.png' alt='1' className='w-full h-full rounded-md' />
-          </div>
+        </div>
 
-          <div className="text-bold mt-3" style={{ fontSize: 10 }}>
-            제품 설명 입력하기
-          </div>
+        <div className="mt-3 grid grid-cols-4 gap-1">
+          <img src='/images/add/result/sampleImage.png' alt='1' className='w-full h-full rounded-md' />
+          <img src='/images/add/result/sampleImage.png' alt='1' className='w-full h-full rounded-md' />
+          <img src='/images/add/result/sampleImage.png' alt='1' className='w-full h-full rounded-md' />
+          <img src='/images/add/result/sampleImage.png' alt='1' className='w-full h-full rounded-md' />
+        </div>
 
-          <textarea
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="w-full mt-2 p-2 border rounded-md"
-            style={{ fontSize: 10, height: 32, resize: 'none', borderColor: '#BFA054', backgroundColor: '#F5EFE1', overflow: 'hidden' }}
-            placeholder="제목을 입력해주세요."
+        <div className="text-bold mt-3" style={{ fontSize: 10 }}>
+          제품 설명 입력하기
+        </div>
 
-          />
+        <textarea
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full mt-2 p-2 border rounded-md"
+          style={{ fontSize: 10, height: 32, resize: 'none', borderColor: '#BFA054', backgroundColor: '#F5EFE1', overflow: 'hidden' }}
+          placeholder="제목을 입력해주세요."
 
-          <textarea
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            className="w-full mt-0.5 p-2 border rounded-md"
-            style={{ fontSize: 10, height: 60, resize: 'none', borderColor: '#BFA054', backgroundColor: '#F5EFE1' }}
-            placeholder={`상세 설명을 입력해 주세요.\nex) 샤넬 스타일로 아우터를 만들어 봤어요 #겨울 아우터 #샤넬스타일 #아우터`}
-          />
+        />
 
-          {/* {imageSrc && (
+        <textarea
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          className="w-full mt-0.5 p-2 border rounded-md"
+          style={{ fontSize: 10, height: 60, resize: 'none', borderColor: '#BFA054', backgroundColor: '#F5EFE1' }}
+          placeholder={`상세 설명을 입력해 주세요.\nex) 샤넬 스타일로 아우터를 만들어 봤어요 #겨울 아우터 #샤넬스타일 #아우터`}
+        />
+
+        {/* {imageSrc && (
           <Image
             src={imageSrc}
             alt="Generated Image"
@@ -203,85 +207,85 @@ function ResultPage() {
           />
         )} */}
 
-          <div
-            className="flex w-full items-center justify-center py-2.5 bg-main-background rounded-md cursor-pointer"
-            // onClick={() => createClothesImage()}
-            style={{ marginTop: 44 }}
-          >
-            <div className="text-white text-bold" style={{ fontSize: 15 }}>
-              등록하기
-            </div>
-
-
+        <div
+          className="flex w-full items-center justify-center py-2.5 bg-main-background rounded-md cursor-pointer"
+          // onClick={() => createClothesImage()}
+          style={{ marginTop: 44 }}
+        >
+          <div className="text-white text-bold" style={{ fontSize: 15 }}>
+            등록하기
           </div>
-        </div>
 
-        {isModalVisible && (
+
+        </div>
+      </div>
+
+      {isModalVisible && (
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={closeModal}
+        >
           <div
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-            onClick={closeModal}
+            className="w-full mx-3 p-2 rounded-2xl bg-main-background"
+            style={{ maxWidth: 343 }}
+          // onClick={(e) => e.stopPropagation()}
+          // onClick={navigateToResultPage}
           >
             <div
-              className="w-full mx-3 p-2 rounded-2xl bg-main-background"
-              style={{ maxWidth: 343 }}
-            // onClick={(e) => e.stopPropagation()}
-            // onClick={navigateToResultPage}
+              className="flex flex-col w-full p-2 rounded-2xl items-center justify-center"
+              style={{ height: 312, border: '2px dashed white' }}
             >
-              <div
-                className="flex flex-col w-full p-2 rounded-2xl items-center justify-center"
-                style={{ height: 312, border: '2px dashed white' }}
-              >
-                <div className="rounded-full" style={{ position: 'relative', width: 96, height: 96, boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-                  <Image
-                    src='/images/add/processModalCircle.svg'
-                    alt='plus'
-                    width={96}
-                    height={96}
-                  />
+              <div className="rounded-full" style={{ position: 'relative', width: 96, height: 96, boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
+                <Image
+                  src='/images/add/processModalCircle.svg'
+                  alt='plus'
+                  width={96}
+                  height={96}
+                />
 
-                  <div
-                    className="w-full h-full text-bold bg-white rounded-full flex items-center justify-center"
-                    style={{
-                      width: 80,
-                      height: 80,
-                      position: 'absolute',
-                      top: '50%',
-                      left: '50%',
-                      transform: 'translate(-50%, -50%)',
-                      fontSize: 25,
-                      color: 'var(--main-color)',
+                <div
+                  className="w-full h-full text-bold bg-white rounded-full flex items-center justify-center"
+                  style={{
+                    width: 80,
+                    height: 80,
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    fontSize: 25,
+                    color: 'var(--main-color)',
 
-                    }}
-                  >
-                    <div>
-                      10%
-                    </div>
+                  }}
+                >
+                  <div>
+                    10%
                   </div>
-
-                </div>
-
-
-                <div
-                  className="text-semiBold text-white mt-4"
-                  style={{ fontSize: 32, color: '#FCFDFE' }}
-                >
-                  옷 제작중
-                </div>
-
-                <div
-                  className="text-medium text-white"
-                  style={{ fontSize: 13, color: '#FCFDFE' }}
-                >
-                  열심히 디자이너님의 옷을 만드는 중이에요!
                 </div>
 
               </div>
 
-            </div>
-          </div>
-        )}
 
-      </div>
+              <div
+                className="text-semiBold text-white mt-4"
+                style={{ fontSize: 32, color: '#FCFDFE' }}
+              >
+                옷 제작중
+              </div>
+
+              <div
+                className="text-medium text-white"
+                style={{ fontSize: 13, color: '#FCFDFE' }}
+              >
+                열심히 디자이너님의 옷을 만드는 중이에요!
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      )}
+
+    </div>
 
   )
 }
