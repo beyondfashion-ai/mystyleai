8982db079a8d057745074568da8da94e949e1756
DaeseungLee <daeseunglee6511@gmail.com>
2024-07-27 15:51:39 +0900

    modify vh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
@@ -10,16 +11,28 @@ export default function Home() {
     router.push('/home');
   };
 
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    setVh();
+    window.addEventListener('resize', setVh);
+
+    return () => {
+      window.removeEventListener('resize', setVh);
+    };
+  }, []);
+
   return (
-    <div className="flex relative flex-col items-center justify-center h-screen">
-      {/* <div className="flex-grow flex flex-col justify-center items-center"> */}
-        <Image
-          src="/images/mainLogo.png"
-          alt="mainLogo"
-          width={144}
-          height={39}
-        />
-      {/* </div> */}
+    <div className="flex relative flex-col items-center justify-center" style={{ height: 'calc(var(--vh, 1vh) * 100)' }}>
+      <Image
+        src="/images/mainLogo.png"
+        alt="mainLogo"
+        width={144}
+        height={39}
+      />
 
       <div
         className="absolute w-full bottom-1 px-5"
@@ -32,20 +45,6 @@ export default function Home() {
           시작하기
         </button>
       </div>
-
-      {/* <div
-        className="w-full px-5"
-        
-      >
-        <button
-          onClick={navigateToHome}
-          className="mb-8 text-bold w-full py-3"
-          style={{ backgroundColor: 'var(--main-color)', borderRadius: 24 }}
-        >
-          시작하기
-        </button>
-      </div> */}
-
     </div>
   );
 }
